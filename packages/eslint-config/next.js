748beab1b76aed88a1ac42ef64e8a80d2ffcae38
egarrisxn <egarrisxn@gmail.com>
2025-03-26 14:30:05 -0400

    boom!

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import globals from "globals";
 import { config as baseConfig } from "./base.js";
 
 /**
@@ -44,6 +44,7 @@ export const nextJsConfig = [
       ...pluginReactHooks.configs.recommended.rules,
       // React scope no longer necessary with new JSX transform.
       "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
 ];
